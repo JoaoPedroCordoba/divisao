c934a708ef399f7cc02933f115283c7aa2c71bf6
JoaoPedroCordoba <jp.8pedro86@gmail.com.br>
2022-04-08 01:48:31 -0300

    finalizado exercicio

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿double numerador, denominador, resultado;
+
+Console.WriteLine("Digite o numerador..:");
+numerador = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Digite o denominador..:");
+denominador = Convert.ToDouble(Console.ReadLine());
+
+if (denominador == 0) 
+    Console.WriteLine("Não é possível dividir por zero.");
+else
+{
+    resultado = numerador / denominador;
+Console.WriteLine($"{numerador} dividido por {denominador} é ={resultado}");
+}
